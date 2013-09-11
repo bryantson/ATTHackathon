63a39b8678c330bb0db9f6f4eb517efcbf7ff1cb
600278 <600278@QJ07300023.corpaa.aa.com>
2013-09-10 20:23:18 -0500

    ADDED a new println message

--- TestGit/src/TestGit.java
+++ TestGit/src/TestGit.java
@@ -3,6 +3,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello, world");
 		System.out.println("Test");
+		System.out.println("Hey, Derek");
 		
 	}
 }
