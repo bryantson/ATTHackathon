45b879e2c56d3e5633796174c73e825d0d22ba6a
600278 <600278@QJ07300023.corpaa.aa.com>
2013-09-12 01:46:13 -0500

    UPDATING the delete of TestGit

--- TestGit/src/TestGit.java
+++
@@ -1,9 +0,0 @@
-
-public class TestGit {
-	public static void main(String[] args) {
-		System.out.println("Hello, world");
-		System.out.println("Test");
-		System.out.println("Hey, Derek");
-		
-	}
-}
